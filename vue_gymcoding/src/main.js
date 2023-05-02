c14d1ac9c39f2af0dbebcee6f79df0210c2a890d
jeonga-Son <jjeong0106@gmail.com>
2023-05-02 23:31:38 +0900

    Vue Router

--- vue_gymcoding/src/main.js
+++ vue_gymcoding/src/main.js
@@ -3,7 +3,8 @@ import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import './assets/main.css'
+import router from '@/router'
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
 
 import 'bootstrap/dist/js/bootstrap.js'
