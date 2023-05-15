5c119ac08a3edf89d65a902eafa2bdd7ac2f9ff6
jeonga-Son <jjeong0106@gmail.com>
2023-05-15 22:25:38 +0900

    use axios

--- vue2_CaptainPangyo_1/vue-form/src/App.vue
+++ vue2_CaptainPangyo_1/vue-form/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- 폼의 기본 동작은 새로고침이다. -->
-  <form v-on:submit="submitForm">
+  <form v-on:submit.prevent="submitForm">
     <div>
       <label for="username">id : </label>
       <input id="username" type="text" v-model="username" />
@@ -14,6 +14,8 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   data: function () {
     return {
@@ -22,9 +24,22 @@ export default {
     };
   },
   methods: {
-    submitForm: function (event) {
-      event.preventDefault();
+    submitForm: function () {
+      // event.preventDefault();
       console.log(this.username, this.password);
+      var url = "https://jsonplaceholder.typicode.com/users";
+      var data = {
+        username: this.username,
+        password: this.password,
+      };
+      axios
+        .post(url, data)
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     },
   },
 };
