450771d519476035792aa90505c0b90a44218a9f
jeonga-Son <jjeong0106@gmail.com>
2023-05-15 22:01:07 +0900

    single file component 5 : emit

--- vue2_CaptainPangyo_1/vue-cli/src/App.vue
+++ vue2_CaptainPangyo_1/vue-cli/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- <app-header v-bind:프롭스 속성 이름="상위 컴포넌트의 데이터 이름"></app-header> -->
-  <app-header v-bind:propsdata="str"></app-header>
+  <app-header v-bind:propsdata="str" v-on:renew="renewStr"></app-header>
 </template>
 
 <script>
@@ -24,6 +24,11 @@ export default {
   },
   components: {
     "app-header": AppHeader,
+  },
+  methods: {
+    renewStr: function () {
+      this.str = "hi";
+    },
   },
 };
 </script>
