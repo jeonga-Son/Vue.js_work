89c6487773cc4a12a5470d35385e8cc23465016a
jeonga-Son <jjeong0106@gmail.com>
2023-05-20 01:20:56 +0900

    update TodoList component

--- vue2_middle_class_CaptainPangyo/vue-todo/src/components/TodoList.vue
+++ vue2_middle_class_CaptainPangyo/vue-todo/src/components/TodoList.vue
@@ -1,13 +1,32 @@
 <template>
-  <div>list</div>
+  <div>
+    <ul>
+      <li v-for="todoItem in todoItems" v-bind:key="todoItem">
+        {{ todoItem }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+  data: function() {
+    return {
+      todoItems: []
+    };
+  },
+  // 생성되는 시점에 created 안의 로직이 한 번 호출된다.
+  created: function() {
+    if (localStorage.length > 0) {
+      for (var i = 0; i < localStorage.length; i++) {
+        if (localStorage.key(i) != "loglevel:webpack-dev-server") {
+          this.todoItems.push(localStorage.key(i));
+          // console.log(localStorage.key(i));
+        }
+      }
+    }
+  }
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
