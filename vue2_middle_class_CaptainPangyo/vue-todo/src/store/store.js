a43412d6a27303f73a71d8f7778f45385207d0a0
jeonga-Son <jjeong0106@gmail.com>
2023-05-24 22:59:36 +0900

    refactor : use state

--- vue2_middle_class_CaptainPangyo/vue-todo/src/store/store.js
+++ vue2_middle_class_CaptainPangyo/vue-todo/src/store/store.js
@@ -4,5 +4,23 @@ import Vuex from "vuex";
 // use => Vue를 사용하는 모든 영역에 특정 기능을 추가하고 싶을 때
 Vue.use(Vuex);
 
+const storage = {
+  fetch() {
+    const arr = [];
+    if (localStorage.length > 0) {
+      for (let i = 0; i < localStorage.length; i++) {
+        if (localStorage.key(i) != "loglevel:webpack-dev-server") {
+          arr.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+        }
+      }
+    }
+    return arr;
+  }
+};
+
 // sotre 변수를 다른 곳에서 사용할 수 있다.
-export const store = new Vuex.Store({});
+export const store = new Vuex.Store({
+  state: {
+    todoItems: storage.fetch()
+  }
+});
