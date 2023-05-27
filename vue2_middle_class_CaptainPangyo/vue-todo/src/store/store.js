afe8c774616b6909cb99aab1a1146b3c1718d64e
jeonga-Son <jjeong0106@gmail.com>
2023-05-27 22:25:29 +0900

    store property modularization

--- vue2_middle_class_CaptainPangyo/vue-todo/src/store/store.js
+++ vue2_middle_class_CaptainPangyo/vue-todo/src/store/store.js
@@ -1,5 +1,7 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import * as getters from "./getters";
+import * as mutations from "./mutations";
 
 // use => Vue를 사용하는 모든 영역에 특정 기능을 추가하고 싶을 때
 Vue.use(Vuex);
@@ -18,38 +20,10 @@ const storage = {
   }
 };
 
-// sotre 변수를 다른 곳에서 사용할 수 있다.
 export const store = new Vuex.Store({
   state: {
     todoItems: storage.fetch()
   },
-  getters: {
-    storedTodoItems(state) {
-      return state.todoItems;
-    }
-  },
-  mutations: {
-    addOneItem(state, todoItem) {
-      const obj = { completed: false, item: todoItem };
-      localStorage.setItem(todoItem, JSON.stringify(obj));
-      state.todoItems.push(obj);
-    },
-    removeOneItem(state, payload) {
-      localStorage.removeItem(payload.todoItem.item);
-      state.todoItems.splice(payload.index, 1);
-    },
-    toggleOneItem(state, payload) {
-      state.todoItems[payload.index].completed = !state.todoItems[payload.index]
-        .completed;
-      localStorage.removeItem(payload.todoItem.item);
-      localStorage.setItem(
-        payload.todoItem.item,
-        JSON.stringify(payload.todoItem)
-      );
-    },
-    clearAllItems(state) {
-      localStorage.clear();
-      state.todoItems = [];
-    }
-  }
+  getters,
+  mutations
 });
