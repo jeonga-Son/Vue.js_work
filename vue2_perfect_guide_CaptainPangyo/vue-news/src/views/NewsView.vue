a4bf68708a487a55154ea72d584ec217e51356ab
jeonga-Son <jjeong0106@gmail.com>
2023-06-01 21:46:05 +0900

    use axios

--- vue2_perfect_guide_CaptainPangyo/vue-news/src/views/NewsView.vue
+++ vue2_perfect_guide_CaptainPangyo/vue-news/src/views/NewsView.vue
@@ -1,9 +1,32 @@
 <template>
-  <div>news</div>
+  <div>
+    <div v-for="(user, index) in users" :key="index">{{ user.title }}</div>
+  </div>
 </template>
 
 <script>
-export default {};
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      users: [],
+    };
+  },
+  created() {
+    var base = this;
+
+    axios
+      .get("https://api.hnpwa.com/v0/news/1.json")
+      .then((response) => {
+        console.log(response);
+        base.users = response.data;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
+};
 </script>
 
 <style></style>
