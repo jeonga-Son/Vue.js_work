a663c35734166a29022c548064a0fde4eb0e6d8b
jeonga-Son <jjeong0106@gmail.com>
2023-06-01 21:07:07 +0900

    use redirect, router-link

--- vue2_perfect_guide_CaptainPangyo/vue-news/src/App.vue
+++ vue2_perfect_guide_CaptainPangyo/vue-news/src/App.vue
@@ -1,26 +1,22 @@
 <template>
   <div id="app">
+    <!--1. Strongly Recommended -->
+    <!-- <ToolBar></ToolBar> -->
+    <!--2. Essential -->
+    <tool-bar></tool-bar>
+
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import ToolBar from "@/components/ToolBar.vue";
+
 export default {
-  methods: {
-    fetchData() {
-      console.log("hello");
-    },
+  components: {
+    ToolBar,
   },
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
