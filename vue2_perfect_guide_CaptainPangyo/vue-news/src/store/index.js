3dd8e58c5ac8ff83fa9a9e1970c23f94597b3b87
jeonga-Son <jjeong0106@gmail.com>
2023-06-05 07:00:19 +0900

    store property modularization

--- vue2_perfect_guide_CaptainPangyo/vue-news/src/store/index.js
+++ vue2_perfect_guide_CaptainPangyo/vue-news/src/store/index.js
@@ -1,8 +1,7 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import { fetchNewsList } from "../api/index.js";
-import { fetchJobsList } from "../api/index.js";
-import { fetchAskList } from "../api/index.js";
+import mutations from "./mutations.js";
+import actions from "./actions.js";
 
 // Vuex는 플러그인 형태로 제공되기 떄문에 Vue.use()를 사용한다.
 Vue.use(Vuex);
@@ -18,45 +17,6 @@ export const store = new Vuex.Store({
       return state.ask;
     },
   },
-  mutations: {
-    SET_NEWS(state, news) {
-      state.news = news;
-    },
-    SET_JOBS(state, jobs) {
-      state.jobs = jobs;
-    },
-    SET_ASK(state, ask) {
-      state.ask = ask;
-    },
-  },
-  actions: {
-    FETCH_NEWS(context) {
-      fetchNewsList()
-        .then((response) => {
-          console.log(response.data);
-          context.commit("SET_NEWS", response.data);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    },
-    FETCH_JOBS({ commit }) {
-      fetchJobsList()
-        .then(({ data }) => {
-          commit("SET_JOBS", data);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    },
-    FETCH_ASK({ commit }) {
-      fetchAskList()
-        .then(({ data }) => {
-          commit("SET_ASK", data);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    },
-  },
+  mutations,
+  actions,
 });
