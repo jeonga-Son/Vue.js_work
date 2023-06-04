91f1a816d033ce67371cd784d012917b3a4d2800
jeonga-Son <jjeong0106@gmail.com>
2023-06-04 10:33:21 +0900

    apply actions & mutations

--- vue2_perfect_guide_CaptainPangyo/vue-news/src/store/index.js
+++ vue2_perfect_guide_CaptainPangyo/vue-news/src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import { fetchNewsList } from "../api/index.js";
 
 // Vuex는 플러그인 형태로 제공되기 떄문에 Vue.use()를 사용한다.
 Vue.use(Vuex);
@@ -7,5 +8,22 @@ Vue.use(Vuex);
 export const store = new Vuex.Store({
   state: {
     news: [],
+  },
+  mutations: {
+    SET_NEWS(state, news) {
+      state.news = news;
+    },
+  },
+  actions: {
+    FETCH_NEWS(context) {
+      fetchNewsList()
+        .then((response) => {
+          console.log(response.data);
+          context.commit("SET_NEWS", response.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
   },
 });
--- vue2_perfect_guide_CaptainPangyo/vue-news/src/views/NewsView.vue
+++ vue2_perfect_guide_CaptainPangyo/vue-news/src/views/NewsView.vue
@@ -1,24 +1,15 @@
 <template>
   <div>
-    <div v-for="(user, index) in users" :key="index">{{ user.title }}</div>
+    <div v-for="(user, index) in this.$store.state.news" :key="index">
+      {{ user.title }}
+    </div>
   </div>
 </template>
 
 <script>
-import { fetchNewsList } from "../api/index.js";
-
 export default {
-  data() {
-    return {
-      users: [],
-    };
-  },
   created() {
-    // 화살표 함수를 쓰게 되면 this를 계속 바인딩 할필요가 없다.
-    // var base = this;
-    fetchNewsList()
-      .then((response) => (this.users = response.data))
-      .catch((error) => console.log(error));
+    this.$store.dispatch("FETCH_NEWS");
   },
 };
 </script>
