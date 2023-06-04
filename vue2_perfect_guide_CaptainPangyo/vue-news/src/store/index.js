03b108d2862fdbc0c012f8f1a3f5df75a102d174
jeonga-Son <jjeong0106@gmail.com>
2023-06-04 10:46:58 +0900

    apply store : JobsView, AskView

--- vue2_perfect_guide_CaptainPangyo/vue-news/src/store/index.js
+++ vue2_perfect_guide_CaptainPangyo/vue-news/src/store/index.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import { fetchNewsList } from "../api/index.js";
+import { fetchJobsList } from "../api/index.js";
+import { fetchAskList } from "../api/index.js";
 
 // Vuex는 플러그인 형태로 제공되기 떄문에 Vue.use()를 사용한다.
 Vue.use(Vuex);
@@ -8,10 +10,18 @@ Vue.use(Vuex);
 export const store = new Vuex.Store({
   state: {
     news: [],
+    jobs: [],
+    asks: [],
   },
   mutations: {
     SET_NEWS(state, news) {
       state.news = news;
+    },
+    SET_JOBS(state, jobs) {
+      state.jobs = jobs;
+    },
+    SET_ASKS(state, asks) {
+      state.asks = asks;
     },
   },
   actions: {
@@ -20,6 +30,24 @@ export const store = new Vuex.Store({
         .then((response) => {
           console.log(response.data);
           context.commit("SET_NEWS", response.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    FETCH_JOBS({ commit }) {
+      fetchJobsList()
+        .then(({ data }) => {
+          commit("SET_JOBS", data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    FETCH_ASKS({commit}) {
+      fetchAskList()
+        .then(({data}) => {
+          commit("SET_ASKS", data);
         })
         .catch((error) => {
           console.log(error);
--- vue2_perfect_guide_CaptainPangyo/vue-news/src/views/JobsView.vue
+++ vue2_perfect_guide_CaptainPangyo/vue-news/src/views/JobsView.vue
@@ -1,22 +1,15 @@
 <template>
   <div>
-    <div v-for="(job, index) in jobList" :key="index">{{ job.title }}</div>
+    <div v-for="(job, index) in this.$store.state.jobs" :key="index">
+      {{ job.title }}
+    </div>
   </div>
 </template>
 
 <script>
-import { fetchJobsList } from "../api/index.js";
-
 export default {
-  data() {
-    return {
-      jobList: [],
-    };
-  },
   created() {
-    fetchJobsList()
-      .then((response) => (this.jobList = response.data))
-      .catch((error) => console.log(error));
+    this.$store.dispatch("FETCH_JOBS");
   },
 };
 </script>
