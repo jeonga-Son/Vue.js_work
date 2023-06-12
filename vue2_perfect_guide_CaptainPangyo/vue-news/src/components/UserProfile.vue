45cfad49db629ee8ce96256e590b7d0662cdb767
jeonga-Son <jjeong0106@gmail.com>
2023-06-13 00:48:48 +0900

    use slot : update userProfile component

--- vue2_perfect_guide_CaptainPangyo/vue-news/src/components/UserProfile.vue
+++ vue2_perfect_guide_CaptainPangyo/vue-news/src/components/UserProfile.vue
@@ -2,10 +2,15 @@
   <div class="user-container">
     <div><i class="fa-solid fa-user"></i></div>
     <div class="user-description">
-      <div>{{ info.id }}</div>
+      <slot name="username">
+        <!-- 상위 컴포넌트에서 정의할 영역 -->
+      </slot>
       <div class="time">
-        {{ info.created }}
+        <slot name="time">
+          <!-- 상위 컴포넌트에서 정의할 영역 -->
+        </slot>
       </div>
+      <slot name="karma"></slot>
     </div>
   </div>
 </template>
--- vue2_perfect_guide_CaptainPangyo/vue-news/src/views/ItemView.vue
+++ vue2_perfect_guide_CaptainPangyo/vue-news/src/views/ItemView.vue
@@ -1,18 +1,13 @@
 <template>
   <div>
     <section>
-      <!-- 질문 상세 정보 -->
-      <div class="user-container">
-        <div><i class="fa-solid fa-user"></i></div>
-        <div class="user-description">
-          <router-link :to="`/user/${fetchedItem.user}`">
-            {{ fetchedItem.user }}
-          </router-link>
-          <div class="time">
-            {{ fetchedItem.time_ago }}
-          </div>
-        </div>
-      </div>
+      <!-- 사용자 정보 -->
+      <user-profile :info="fetchedItem">
+        <div slot="username">{{ fetchedItem.user }}</div>
+        <template slot="time">{{ fetchedItem.time_ago }}</template>
+      </user-profile>
+    </section>
+    <section>
       <h2>{{ fetchedItem.title }}</h2>
     </section>
     <section>
@@ -24,8 +19,12 @@
 
 <script>
 import { mapGetters } from "vuex";
+import UserProfile from "../components/UserProfile.vue";
 
 export default {
+  components: {
+    UserProfile,
+  },
   computed: {
     ...mapGetters(["fetchedItem"]),
   },
