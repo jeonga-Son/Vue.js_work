f2993741020a7045d1491aa4b844b1bc0347e30d
jeonga-Son <jjeong0106@gmail.com>
2023-06-14 00:51:45 +0900

    style : update UserProfile component

--- vue2_perfect_guide_CaptainPangyo/vue-news/src/components/UserProfile.vue
+++ vue2_perfect_guide_CaptainPangyo/vue-news/src/components/UserProfile.vue
@@ -5,12 +5,14 @@
       <slot name="username">
         <!-- 상위 컴포넌트에서 정의할 영역 -->
       </slot>
-      <div class="time">
+      <span class="time">
         <slot name="time">
           <!-- 상위 컴포넌트에서 정의할 영역 -->
         </slot>
-      </div>
-      <slot name="karma"></slot>
+      </span>
+      <slot name="karma">
+        <!-- 상위 컴포넌트에서 정의할 영역 -->
+      </slot>
     </div>
   </div>
 </template>
