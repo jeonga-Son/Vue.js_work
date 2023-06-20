c175b6705b14b9e10b9b2bd3b288e422e9491175
jeonga-Son <jjeong0106@gmail.com>
2023-06-20 23:44:05 +0900

    refactor : use Mixin

--- vue2_perfect_guide_CaptainPangyo/vue-news/src/routes/index.js
+++ vue2_perfect_guide_CaptainPangyo/vue-news/src/routes/index.js
@@ -1,8 +1,11 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import NewsView from "../views/NewsView.vue";
+import AskView from "../views/AskView.vue";
+import JobsView from "../views/JobsView.vue";
 import ItemView from "../views/ItemView.vue";
 import UserView from "../views/UserView.vue";
-import CreateListView from "../views/CreateListView.js";
+// import CreateListView from "../views/CreateListView.js";
 
 Vue.use(VueRouter);
 
@@ -18,17 +21,20 @@ export const router = new VueRouter({
     {
       path: "/news",
       name: "news",
-      component: CreateListView("NewsView"),
+      // component: CreateListView("NewsView"),
+      component: NewsView,
     },
     {
       path: "/ask",
       name: "ask",
-      component: CreateListView("AskView"),
+      // component: CreateListView("AskView"),
+      component: AskView,
     },
     {
       path: "/jobs",
       name: "jobs",
-      component: CreateListView("JobsView"),
+      // component: CreateListView("JobsView"),
+      component: JobsView,
     },
     {
       path: "/item/:id",
