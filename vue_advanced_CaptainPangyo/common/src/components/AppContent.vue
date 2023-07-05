9c3c4ea63a104151d216585406b2e2781c89bdb2
jeonga-Son <jjeong0106@gmail.com>
2023-07-05 23:57:45 +0900

    Common Approach

---
+++ vue_advanced_CaptainPangyo/common/src/components/AppContent.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="(item, index) in items" :key="index">
+        {{ item }}
+      </li>
+    </ul>
+    <button @click="$emit('renew')">renew items</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    items: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style></style>
