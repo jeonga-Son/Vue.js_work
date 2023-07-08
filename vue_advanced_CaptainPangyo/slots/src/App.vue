2fc738188e93d8c34c673693f1166a50eb9dd14c
jeonga-Son <jjeong0106@gmail.com>
2023-07-08 23:52:59 +0900

    Component with Slots

---
+++ vue_advanced_CaptainPangyo/slots/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <ul>
+      <item>아이템 1</item>
+      <item>아이템 2 <button>click me</button></item>
+      <item>
+        <div>아이템 3</div>
+        <img
+          src="https://cdn.pixabay.com/photo/2021/02/27/17/46/orangutan-6055309_1280.jpg"
+          alt="오랑이"
+          width="50px"
+          height="50px"
+        />
+      </item>
+      <item>
+        <div style="color: blue; font-size: 20px">아이템 4</div>
+      </item>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Item from "./Item.vue";
+
+export default {
+  components: {
+    Item,
+  },
+};
+</script>
